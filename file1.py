75d070588179d5c1430b82129530e4e3843acb13
virtual PC <huxinsheng2015@icloud.com>
2025-09-11 12:02:04 +0800

    安装mypy ,ruff ;  pip freeze again

--- file1.py
+++ file1.py
@@ -1,9 +1,15 @@
-
 # 请从这里开始
 
-import datetime #ignore
-import json #ignore
+import datetime  # ignore
+import json  # ignore
 
 
+def add(num1, num2):
+    """
+    returns the sum of two numbersre
+    """
+
+    return num1 + num2
 
 
+print(add(1, 2))  # 3
